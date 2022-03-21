1dc3ef658f8a550176f30800c10ed78c0980361f
Thanh Ngoc <ngochc1@gmail.com>
2022-03-21 14:07:24 +0700

    edit floor

--- src/components/Floor.jsx
+++ src/components/Floor.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {  useBox } from '@react-three/cannon'
 export default (props) => {
-  const [ref,api] = useBox(()=>({args: [20,1,10],...props}));
+  const [ref,api] = useBox(()=>({args: [50,20,50],...props}));
     return (
       <mesh castShadow
        receiveShadow ref={ref} {...props} >
         <boxBufferGeometry args={[50, 20, 50]}></boxBufferGeometry>
-        <meshPhysicalMaterial transparent opacity={0.2} ></meshPhysicalMaterial>
+        <meshPhysicalMaterial  opacity={0.2} ></meshPhysicalMaterial>
       </mesh>
  )
 }
